388fa8edfbb9cc2fb07a4de21d79fdb58540778e
YasanayakeR <yasanayakahkarg.22@uom.lk>
2025-03-29 15:36:08 +0530

    pagination component added

---
+++ src/app/components/shared/pagination/pagination.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css'
+})
+export class PaginationComponent {
+  totalPages = 3; 
+  currentPage = 1;
+
+  setPage(page: number) {
+    this.currentPage = page;
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+}
