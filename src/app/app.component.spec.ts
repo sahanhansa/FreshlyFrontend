f9776f249d6e0f0b95d5989d107b4fe4a11aac4c
ManukaPathirana <manuka0405@gmail.com>
2025-05-04 13:40:55 +0530

    minnor changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AngularAuthUI' title`, () => {
+  it(`should have the 'FreshlyFrontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularAuthUI');
+    expect(app.title).toEqual('FreshlyFrontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularAuthUI');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FreshlyFrontend');
   });
 });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter, Routes } from '@angular/router';
+// import { LaundryPageComponent } from './pages/order/laundry-list-page/laundry-list-page.component';
+// import { ItemsListPageComponent } from './pages/order/items-list-page/items-list-page.component';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// export const routes: Routes = [
+//   { path: '', component: LaundryPageComponent },
+//   { path: 'items/:id', component: ItemsListPageComponent }]; // Route with laundry ID
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-
-
 export const appConfig: ApplicationConfig = {
+  // providers: [
+  //   provideRouter(routes),
+  //   // other providers...
+  // ],
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),provideHttpClient()]
 };
