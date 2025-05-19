87c41dd972e4f88210c1801904c71c3c6497f921
YasanayakeR <yasanayakahkarg.22@uom.lk>
2025-05-19 18:56:39 +0530

    merged laundry & driver

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,26 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/customer/login/login.component';
 import { SignupComponent } from './components/customer/signup/signup.component';
 import { WhoAreYouComponent } from './components/customer/whoareyou/whoareyou.component';
-import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { RouterModule} from '@angular/router';
 import { HomeComponent } from './pages/Laundry/home/home.component';
-=======
-import { RouterModule } from '@angular/router';
-
-
-
-//driver routes
+import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 import { DriverHomePageComponent } from './pages/Driver/driver-home-page/driver-home-page.component';
 import { PickupsTasksMainpageComponent } from './pages/Driver/pickups-tasks-mainpage/pickups-tasks-mainpage.component';
 import { PickupsPendingOrderDetailsComponent } from './pages/Driver/pickups-pending-order-details/pickups-pending-order-details.component';
 import { DeliveryTasksMainpageComponent } from './pages/Driver/delivery-tasks-mainpage/delivery-tasks-mainpage.component';
 import { DeliveryPendingOrderDetailsComponent } from './pages/Driver/delivery-pending-order-details/delivery-pending-order-details.component';
 import { DriverContactusPageComponent } from './pages/Driver/driver-contactus-page/driver-contactus-page.component';
-
-
-import { LaundryHomeComponent } from './components/customer/laundry-home/laundry-home.component';
->>>>>>> b0271b6d6c5d7bd609c8896f7c888e0e493386b5
 import { AdminHomeComponent } from './components/admin-home/admin-home.component';
 import { CusHomeComponent } from './components/customer/cus-home/cus-home.component';
 import { LaundrySignupComponent } from './components/customer/laundry-signup/laundry-signup.component';
@@ -62,8 +51,6 @@ export const routes: Routes = [
  // Route for how-it-works
    {path: 'how-it-works', component:HowItWorksComponent},
 
-
-<<<<<<< HEAD
   // Route for new orders page
   {path: 'new-orders', component:NewOrdersComponent},
 
@@ -72,7 +59,7 @@ export const routes: Routes = [
 
   // Route for invoice success page
    {path: 'invoice-successed', component:InvoiceSuccessedComponent},
-=======
+
   //driver routes
   {path: 'driver-home-page', component:DriverHomePageComponent },
   {path: 'pickups-tasks-mainpage', component: PickupsTasksMainpageComponent },
@@ -80,19 +67,13 @@ export const routes: Routes = [
   {path: 'driver-contactus-page', component: DriverContactusPageComponent},
   {path: 'order-details-pending/:id', component: PickupsPendingOrderDetailsComponent}, 
   {path: 'delivery-order-details/:id', component: DeliveryPendingOrderDetailsComponent},
-  
-  
 
-   
-    
-  
-    
->>>>>>> b0271b6d6c5d7bd609c8896f7c888e0e493386b5
 ];
 // Define the routing module
-  @NgModule({
+ @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
   })
   export class AppRoutingModule {}
+
 
